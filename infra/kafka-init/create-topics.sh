9ce2c8862dc738c1395877de0364e2a28be69761
Anish Kalra <anishkalra05@gmail.com>
2026-01-05 14:48:39 -0600

    fixing yaml file for kafka topics

--- infra/kafka-init/create-topics.sh
+++ infra/kafka-init/create-topics.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
 set -e
 
 echo "Creating Kafka topics..."
 
-kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists \
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists \
   --topic events.raw --partitions 6 --replication-factor 1
 
-kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists \
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --create --if-not-exists \
   --topic events.dlq --partitions 3 --replication-factor 1
 
 echo "Topics created successfully."
-
